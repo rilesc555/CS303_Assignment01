4ad6b65bd17753aec5af59396870bf85159c511a
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-09-05 14:54:18 -0500

    Added code in main to read from file to vector

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
     cout << "Assignment 1" << endl;
+
+    vector<int> intVector;
+
+    ifstream input;
+    input.open("input.txt");
+    while (!input.is_open()) {
+        cout << "Unable to open \"input.txt\"\n";
+        cout << "Please enter a new file name: ";
+        string tempString;
+        getline(cin, tempString);
+        input.open(tempString);
+    }
+
+    while (!input.eof()) {
+        int tempInt;
+        input >> tempInt;
+        intVector.push_back(tempInt);
+    }
+    input.close();
+
     return 0;
 }
